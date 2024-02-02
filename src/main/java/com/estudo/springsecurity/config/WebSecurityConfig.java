bc4e044491b47498c772c0f19dcb753dd457c434
frederico <fredyramos0707@gmail.com>
2024-02-02 16:26:22 -0300

    Autenticacao em memoria com usuario e senha. Tomou erro porque a senha nao foi passada com encoder.

--- src/main/java/com/estudo/springsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/estudo/springsecurity/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.estudo.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -19,10 +20,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			 *      permiteAll deixa acessar todos endpoints.
 			*/
 			/*.authenticated() ==> permite acesso aos endpoints somente de usuarios autenticados.*/
-			.permitAll()
+			.authenticated()
 			.and()
 			.csrf().disable();
 		    /* Veja que mesmo sem passar autenticacao no postman, a requisicao POST deu certo
 		          porque o csrf foi desabilitado, e está sendo usado junto com .permiteAll. */
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+				.withUser("frederico")
+				.password("123456")
+				.roles("ADMIN");
 	}
 }
