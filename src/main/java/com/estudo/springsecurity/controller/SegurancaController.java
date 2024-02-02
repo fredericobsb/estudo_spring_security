ae7e123b7288bcc2a310ffbb454b5aa33a90baae
frederico <fredyramos0707@gmail.com>
2024-02-02 15:26:22 -0300

    Aplicacao permitindo acessar todos endpoints. Ainda sem a dependencia do spring security.

---
+++ src/main/java/com/estudo/springsecurity/controller/SegurancaController.java
@@ -0,0 +1,26 @@
+package com.estudo.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudo.springsecurity.dto.CadastradorDto;
+
+@RestController
+@RequestMapping(value="/api/v1/teste",  produces = {"application/json"})
+public class SegurancaController {
+
+	@PostMapping
+	public String cadastradorDto(@RequestBody CadastradorDto dto) {
+		System.out.println("Nome que veio no dto ==> " + dto.getNome());
+		System.out.println("CPF que veio no dto ==> " + dto.getCpf());
+		return " ===> @PostMapping cadastrador => nome =" + dto.getNome();
+	}
+	
+	@GetMapping
+	public String recuperadorDto() {
+		return " ===> @GetMapping recuperadorDto()";
+	}
+}
---
+++ src/main/java/com/estudo/springsecurity/dto/CadastradorDto.java
@@ -0,0 +1,10 @@
+package com.estudo.springsecurity.dto;
+
+import lombok.Data;
+
+@Data
+public class CadastradorDto {
+
+	private String nome;
+	private String cpf;
+}
